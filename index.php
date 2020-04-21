e769db308fc77ea12f2d4a4e4a93753b85dd8691
afyki <40294773+afyki@users.noreply.github.com>
2020-04-21 20:26:48 +0900

    Update index.php

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if ($close){
   <title>就プレ</title>
   <!-- drawer.css -->
   <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Noto+Sans+JP:400,700" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="./css/index.css">
+  <link rel="stylesheet" type="text/css" href="./css/index0.css">
   <!-- jquery & iScroll -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js"></script>
